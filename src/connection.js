eb28c8d2c55e55e920f2727932950d67d62407d5
Carlos Pessoa <carlos.henriquepessoa15@gmail.com>
2023-03-15 18:16:07 -0300

    escopo da api e configuração para funcionamento local feito, schema do banco de daodos criado

---
+++ src/connection.js
@@ -0,0 +1,9 @@
+module.exports = require("knex")({
+    client: "pg",
+    connection: {
+        host: 'localhost',
+        user: 'postgres',
+        password: '123456',
+        database: 'dindin'
+    },
+});
